1712b014309b888da91e7d7c602162fa26d65852
Conor Byrne <conor.byrne@mymail.champlain.edu>
2023-10-11 14:31:16 -0400

    finished the website for week 6

--- week6/makeWeb.bash
+++ week6/makeWeb.bash
@@ -4,25 +4,32 @@ theTime=""
 requested=""
 user=""
 
+echo "" > web.html
+
 echo "<html>" > web.html
 echo "<body>" >> web.html
 echo "<table>" >> web.html
 echo "<tr>" >> web.html
-echo "<th>IP</th>" >> web.hmtl
+echo "<th>IP</th>" >> web.html
 echo "<th>Time</th>" >> web.html
 echo "<th>Requested Page</th>" >> web.html
 echo "<th>User Agent</th>" >> web.html
-echo "</tr>" >> web.hmtl
+echo "</tr>" >> web.html
 while read -r line
 do
 	echo "<tr>" >> web.html
-	ip="${line}" | cut -d' ' -f1
-	echo "<td>""${ip}""</td>" >> web.html
-	theTime="${line}" | cut -d' ' -f4
-	echo "<td>""${time}""</td>" >> web.html
-#	requested="${line}" | cut
+	ip=$(echo "${line}" | cut -d ' ' -f1)
+	echo "<td>${ip}</td>" >> web.html
+	theTime=$(echo "${line}" | cut -d ':' -f2,3)
+	echo "<td>${theTime}</td>" >> web.html
+	requested=$(echo "${line}" | cut -d ' ' -f7)
+	echo "<td>${requested}</td>" >> web.html
+	user=$(echo "${line}" | cut -d ' ' -f12)
+	echo "<td>${user}</td>" >> web.html
 	echo "</tr>" >> web.html
 done < "$log"
 echo "</table>" >> web.html
 echo "</body>" >> web.html
 echo "</html>" >> web.html
+
+cp web.html /var/www/html/web.html
