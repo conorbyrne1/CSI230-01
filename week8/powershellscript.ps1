b01a42873b6e8418ee6d32da45e339be851ff11b
conorbyrne1 <conor.byrne@mymail.champlain.edu>
2023-10-18 14:58:58 -0400

    forgot to save the script the first time around

---
+++ week8/powershellscript.ps1
@@ -0,0 +1,9 @@
+﻿cd C:\Users\champuser\CSI230-01\week8\
+
+clear
+
+Get-EventLog -list
+
+$readLog = Read-Host -Prompt "Please select a Log to export"
+
+Get-EventLog -LogName $readLog -Newest 3 | export-csv -NoTypeInformation -Path "C:\Users\champuser\CSI230-01\week8\rep.csv"
