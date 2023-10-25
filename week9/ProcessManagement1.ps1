70ea2b72cb3f20d6e024ffcb9e2fc3538e71f438
conorbyrne1 <conor.byrne@mymail.champlain.edu>
2023-10-25 15:16:22 -0400

    started working on assignment

---
+++ week9/ProcessManagement1.ps1
@@ -0,0 +1,14 @@
+﻿#part1
+$cProg=Get-Process -Name C* | Where-Object Name -ilike 'C*' | select Id, ProcessName, StartTime
+Write-Host $cProg
+Write-Host "---"
+
+$date=Get-Date
+#if($cProg StartTime -ge $date - 1 ) {
+Write-Host $date
+Write-Host "---"
+
+$cTime="10/25/2023 2:30:00 PM"
+
+Write-Host "---"
+Get-Process -Name C* | Where-Object Name -ilike 'C*' | Where-Object StartTime -GT $cTime | select Id, ProcessName, StartTime
