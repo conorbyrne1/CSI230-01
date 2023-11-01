0ee91b69a6dc42ab581ba705a41844acde83383c
conorbyrne1 <conor.byrne@mymail.champlain.edu>
2023-11-01 14:32:39 -0400

    Line/Word count

---
+++ week10/LineCounter.ps1
@@ -0,0 +1,7 @@
+﻿$FilesToLookAt = Get-ChildItem -Recurse -Include *.bash
+
+#$FilesToLookAt
+
+for($i=0; $i -lt $FilesToLookAt.Count; $i++){
+Get-Content $FilesToLookAt[$i] | Measure-Object -Line -Word
+}
