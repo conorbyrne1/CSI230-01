ee8a10c6763b123e02f47c64bc89738313ee4413
conorbyrne1 <conor.byrne@mymail.champlain.edu>
2023-11-29 14:00:01 -0500

    week 14 assignment

---
+++ week12/Windows Apache Logs.ps1
@@ -0,0 +1,42 @@
+﻿clear
+
+#list all of the apaches logs of xampp
+#Get-Content C:\xampp\apache\logs\access.log -Tail 5
+
+#list all of the apaches logs of xampp
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 ',' 400 '
+
+#list all of the apaches logs of xampp excluding 200 
+$B = Get-Content C:\xampp\apache\logs\access.log | Select-String ' 200 ' -NotMatch
+
+#From every .log file in the directory, only get logs that contain the word 'error'
+$A = Get-Content C:\xampp\apache\logs\*.log | Select-String -AllMatches 'error'
+#print last 5
+#$A[-5..-1]
+
+$regex = [regex] `
+"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+$ips = $regex.Matches($B) | select @{Name="IP"; Expression={ $_.value }}
+#$ips
+
+$counts = $ips | Group-Object IP -NoElement
+#$counts
+
+$countsDetailed = $counts | Select-Object Count, Name, `
+@{Name="Decision"; Expression={if($_.Count -gt 3) {"Abnormal"} `
+                              else {"Normal"} `
+                               }}
+#$countsDetailed
+
+#block ips if ip is abnormal
+foreach($ip in $countsDetailed){
+
+    if([string]$ip.Decision -ilike "Abnormal") {
+    Write-Host "Blocking $ip.Name"
+    New-NetFirewallRule -DisplayName "Bad IPs $ip.Name" `
+    -Direction Inbound -Action Block -RemoteAddress $ip.Name
+    }
+
+}
+#remove created firewall rules
+Remove-NetFirewallRule -DisplayName "Bad IPs*"
---
+++ week14/WebScraping.ps1
@@ -0,0 +1,58 @@
+﻿clear
+
+$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://192.168.3.29/ToBeScrapped.html
+
+# Get a count of the links in the page
+#$scraped_page.Links.Count
+
+# Display links as HTML elements
+#$scraped_page.Links
+
+# Display only URL and its text
+#$scraped_page.Links | where { $_.href -ilike "*https*" } | select href
+
+$h2s=$scraped_page.ParsedHtml.body.getElementsByTagName("h2") #| select outerText
+
+#for($i=0; $i -lt $h2s.Length; $i++){
+#$h2s[$i].getElementsByTagName("a") | select `
+#@{Name="h2 element"; Expression={$i}}, outerText, href
+#}
+
+# Print innerText of every div element that has the class as "div-1"
+$divs1=$scraped_page.ParsedHtml.getElementsByTagName("div") | where { `
+$_.getAttributeNode("class").Value -ilike "*div-1*" } | select innerText
+
+#$divs1
+
+#$trs=$scraped_page.ParsedHtml.body.getElementsByTagName("tr")
+
+#$multirow = @()
+#for($i=1; $i -lt $trs.length; $i++){
+#$tds=$trs[$i].getElementsByTagName("td")
+
+#$singlerow = @(
+#@{Row=$i; Employee=$tds[0].innerText; Department=$tds[1].innerText; `
+#Salary=$tds[2].innerText}
+#)
+#$multirow + $singlerow
+#}
+#$multirow
+
+#calc avg salary
+$avg=0
+
+#for($i=1; $i -lt $trs.length; $i++){
+#$tds=$trs[$i].getElementsByTagName("td")
+#$avg+=$tds[2].innerText
+#}
+#$avg=$avg/3
+
+#echo "Average Salary is" $avg
+
+$alldivs=$scraped_page.ParsedHtml.body.getElementsByTagName("div")
+Foreach($alldiv in $alldivs)
+{
+    $alldiv.id
+}
+
+#$alldivs
